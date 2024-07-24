b9d60b25855228d59acb5957c73a446a16d87799
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2024-07-23 23:31:36 -0300

    Add contact session

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica para enviar os dados do formulário
+    alert('Formulário enviado com sucesso!');
+});
