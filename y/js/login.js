5df8252fc9198ee84bc504a7e4805a78b0ef860b
Gustavo Batista <guuhjb@gmail.com>
2024-08-21 20:35:37 -0300

    cadastrando produtos

--- y/js/login.js
+++ y/js/login.js
@@ -7,27 +7,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        // Autentica com Firebase
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
-
-                // Verifica se o usuário é um administrador
                 if (email === 'admin@admin.com' && password === 'admin1234') {
-                    // Redireciona para a página de administração
                     alert('Login de administrador realizado com sucesso!');
                     window.location.href = 'admin.html';
                 } else {
-                    // Verifica se o usuário existe no Firestore
                     const db = firebase.firestore();
                     db.collection("Pessoas").where("email", "==", email).get()
                         .then((querySnapshot) => {
                             if (!querySnapshot.empty) {
-                                // Usuário encontrado no Firestore
                                 alert('Login realizado com sucesso!');
-                                window.location.href = 'index.html'; // Redireciona para a página principal
+                                window.location.href = 'index.html'; 
                             } else {
-                                // Usuário não encontrado no Firestore
                                 alert('Usuário não encontrado no Firestore.');
                             }
                         })
